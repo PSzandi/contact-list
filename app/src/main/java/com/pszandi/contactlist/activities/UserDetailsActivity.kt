e1047882f6370897e7c7d37a029793f531a7f545
Sándor Uszkai <uszkai.sandor@codeyard.eu>
2021-04-10 14:21:07 +0200

    add ViewBinding to UserDetailsActivity

--- app/src/main/java/com/pszandi/contactlist/activities/UserDetailsActivity.kt
+++ app/src/main/java/com/pszandi/contactlist/activities/UserDetailsActivity.kt
@@ -3,13 +3,17 @@ package com.pszandi.contactlist.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pszandi.contactlist.data.User
+import com.pszandi.contactlist.databinding.ActivityUserDetailsBinding
 
-class UserDetailsActivity: AppCompatActivity() {
+class UserDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo csináld meg a layout inflate-elést a viewBindinggal, és add át a setContentView-ban.
-        // lesz rá példa a UserListActivity-ben
+        binding = ActivityUserDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         // Kiszedjük az intent-ből a user-t, amit az előző activity-ről adtunk át.
         val user: User? = intent.extras?.getSerializable(UserListActivity.USER) as? User?
@@ -20,7 +24,5 @@ class UserDetailsActivity: AppCompatActivity() {
         super.onStart()
         // todo ide kell majd a nézet logika
     }
-
-    // Todo készíts egy activity-t, az elküldött kép alapján, a user objektumból építkezve
 
 }
