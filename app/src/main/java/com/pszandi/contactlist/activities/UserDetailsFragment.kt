4cfcce33e36c7ae24eae54d15a9e8def8e0df028
Alexandra Paksi <paksi.szandi04@gmail.com>
2021-04-18 16:00:40 +0300

    Refactor UserListDetailsActivity to fragment

--- app/src/main/java/com/pszandi/contactlist/activities/UserDetailsFragment.kt
+++ app/src/main/java/com/pszandi/contactlist/activities/UserDetailsFragment.kt
@@ -1,38 +1,43 @@
 package com.pszandi.contactlist.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import coil.load
-
 import com.pszandi.contactlist.data.User
-import com.pszandi.contactlist.databinding.ActivityUserDetailsBinding
+import com.pszandi.contactlist.databinding.FragmentUserDetailsBinding
 
 
-class UserDetailsActivity : AppCompatActivity() {
+class UserDetailsFragment : Fragment() {
 
-    private lateinit var binding: ActivityUserDetailsBinding
+    private lateinit var binding: FragmentUserDetailsBinding
     var user: User? = null
     //var phoneIcon : String = "https://www.pngegg.com/en/png-xixwv"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUserDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
         // Kiszedjük az intent-ből a user-t, amit az előző activity-ről adtunk át.
-        user = intent.extras?.getSerializable(UserListFragment.USER) as? User?
-        // todo itt már van usered, onStartban ezt tudod majd használni.
+        //user = intent.extras?.getSerializable(UserListFragment.USER) as? User?
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserDetailsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        // todo ide kell majd a nézet logika
         binding.cardImage.load(user?.image)
         binding.userName.text = "${user?.name?.firstName} ${user?.name?.lastName}"
         binding.tvDetails.text = "${user?.phoneNumber}"
-       
+
     }
 
 }
